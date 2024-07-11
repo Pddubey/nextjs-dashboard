4fe3e9e319309d4a535b38e7d838bbcb91f20b05
priyanshu Dubey <pd5108445@gmail.com>
2024-07-11 19:00:21 +0530

    Initial Commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const page = () => {
+  return (
+    <div>
+      <p> Hello from customer </p>
+    </div>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <p className='text-blue-500'>Hello from Invoice</p>
+    </div>
+  )
+}
+
+export default page
